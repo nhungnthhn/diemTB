b4f21e784aab66f722c109c03e13dcee50257e83
nhungnthhn <86668997+nhungnthhn@users.noreply.github.com>
2021-07-12 21:46:13 +0700

    tinh tong

---
+++ tinhtong.js
@@ -0,0 +1,16 @@
+function tinhtong() {
+//  lay thong tin diem tu nguoi dung
+    let x = parseFloat(prompt ("Hãy nhập điểm Toán"));
+    let y = parseFloat(prompt ("Hãy nhập điểm Lý"));
+    let z = parseFloat(prompt ("Hãy nhập điểm Hóa"));
+
+// tinh tong
+    let tong = x + y + z;
+    let m = parseFloat(tong); tong = Math.round(m * 100)/100;
+    let TB = tong/3;
+    let n = parseFloat(TB); TB = Math.round(n * 100)/100;
+
+// In kết quả ra màn hình
+    document.write("Điểm tổng nà: " + tong + "<br>");
+    document.write("Điểm TB xún dòng nà: "+ TB)
+}
